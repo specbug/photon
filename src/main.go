d8d8c1f082c7ebe36d4d04ad013b945840749044
Rishit Vora <rishit7.rv@gmail.com>
2024-10-21 17:22:14 +0530

    remove parser

--- src/main.go
+++ src/main.go
@@ -3,7 +3,6 @@ package main
 import (
 	"fmt"
 	"os"
-	"photon/src/parser"
 	"photon/src/tokenizer"
 )
 
@@ -15,13 +14,6 @@ func main() {
 
 	fileName := os.Args[1]
 	fileContent, err := os.ReadFile(fileName)
-	if err != nil {
-		fmt.Println(err)
-		os.Exit(1)
-	}
-
-	baseParser := parser.BaseParser{FileContent: &fileContent}
-	err = baseParser.Parse()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
